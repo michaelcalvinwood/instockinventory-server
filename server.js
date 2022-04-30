1688ad9055bec6282fe90e657544a749d070f5f5
Michael Wood <michaelwood33311@icloud.com>
2022-04-30 13:04:25 -0400

    added https server for cloud deployment

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+const DOMAIN = 'instockinventory.com';
+const PORT = process.env.PORT || 8080;
+
 require('dotenv').config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
-const PORT = process.env.PORT || 8080;
+const https = require('https');
+const fs = require('fs');
 
 // Middleware
+app.use((req, res, next) => {
+    console.log(req.url);
+    next();
+});
+
 app.use(express.static('public'))
 app.use(express.json())
 app.use(cors());
@@ -17,4 +25,11 @@ const inventoryRoute = require('./routes/inventory');
 app.use('/inventory', inventoryRoute);
 
 // Listen
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+const httpsServer = https.createServer({
+    key: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/privkey.pem`),
+    cert: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/fullchain.pem`),
+   }, app);
+   
+httpsServer.listen(PORT, () => {   
+    console.log(`HTTPS Server running on port ${PORT}`);
+});
